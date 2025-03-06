51c60512e3d7a9890ade8e7448970df74b096cf6
Nawabul <nawabk693@gmail.com>
2025-03-06 16:23:21 +0530

    Add: Methods of tree

--- javascript/tree/binary_tree.js
+++ javascript/tree/binary_tree.js
@@ -1,4 +1,5 @@
-
+// import { QueueUsingLL } from "../Queue/queue_using_link_list";
+const QueueUsingLL = require('../Queue/queue_using_link_list')
 class Node {
 
 	data;
@@ -14,14 +15,34 @@ class Tree {
 
 	constructor(array){
 		const root = this.buildTree(array)
-		console.log(root.data)
-		// pre order traversal
-		console.log("Pre Order Traversal")
-		this.preOrderTraversal(root);
+		// console.log(root.data)
+		// // pre order traversal
+		// console.log("Pre Order Traversal")
+		// this.preOrderTraversal(root);
 
-		// in order traversal
-		console.log('in order traversal')
-		this.inOrderTraversal(root);
+		// // in order traversal
+		// console.log('in order traversal')
+		// this.inOrderTraversal(root);
+
+		// // post order traversal 
+		// console.log("Post Order traversal")
+		// this.postOrderTraversal(root)
+
+		// get tree level
+		// console.log('tree level')
+		// this.treeLevel(root)
+
+		// // count
+		// console.log('count')
+		// console.log(this.count(root,0))
+
+		// sum
+		const sum = this.sum(root);
+		console.log('sum : ',sum)
+		// sum
+		const hight = this.hight(root);
+		console.log('Hight : ',hight)
+		;
 	}
 
 	index=-1
@@ -82,6 +103,118 @@ class Tree {
 		console.log(data)
 		this.inOrderTraversal(rightNode)
 
+	
+	}
+
+	// post order traversal 
+	postOrderTraversal(root){
+
+		// base casd 
+		if(!root){
+			return 
+		}
+
+		const data = root.data
+		const leftNode = root.left
+		const rightNode = root.right
+
+		this.postOrderTraversal(leftNode)
+		this.postOrderTraversal(rightNode)
+		console.log(data)
+
+	}
+
+	// get tree level
+
+	treeLevel(root){
+
+		if(!root){
+			return 
+		}
+
+		// create queue 
+		const queue =  new QueueUsingLL()
+
+	
+		let level = 0;
+		queue.push(root)
+		// push nulll
+		queue.push(null);
+
+		while(!queue.isEmpty()){
+
+			let currentNode = queue.pull();
+
+			if(currentNode === null){
+				// level up
+				level++;
+				console.log("level :",level)
+				if(queue.isEmpty()){
+					break
+				}else{
+					queue.push(null)
+				}
+			}else{
+				
+				console.log( currentNode.data)
+				// push children
+				if(currentNode.left != null){
+
+					queue.push(currentNode.left)
+				}
+				if(currentNode.right != null){
+
+					queue.push(currentNode.right)
+				}
+			
+			}
+
+
+		}
+
+		console.log('level ',level)
+
+
+
+	}
+
+	count(root){
+		if(root === null){
+			return 0;
+		}
+		
+
+		const left = this.count(root.left,i)
+		const right = this.count(root.right,i)
+		return left + right + 1;
+	}
+
+	sum(root){
+
+		if(!root){
+			return 0;
+		}
+
+
+		const currentNumber = root.data;
+		const leftNumber = this.sum(root.left)
+		const rightNumber = this.sum(root.right)
+		
+		return currentNumber + leftNumber + rightNumber
+	}
+
+	hight(root){
+
+		if(!root){
+			return 0;
+		}
+
+		const leftHight = this.hight(root.left)
+		const rightHight = this.hight(root.right)
+
+		const maxHight = Math.max(leftHight,rightHight) + 1;
+		return maxHight
+	
 	
 	}
 
